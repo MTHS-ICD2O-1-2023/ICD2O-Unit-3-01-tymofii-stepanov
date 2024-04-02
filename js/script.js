e1d60d28eac45f23766e00e5c320b9de92809671
Tymofii <159185555+tymofii-stepanov@users.noreply.github.com>
2024-04-02 18:49:36 +0000

    End with work

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Tymofii
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates Area Of Trapezoid
+ */
+function calculateAreaOfTrapezoid() {
+  // input
+  const aBase = parseInt(document.getElementById('a-base').value)
+  const bBase = parseInt(document.getElementById('b-base').value)
+  const height = parseInt(document.getElementById('height-mm').value)
+
+
+  // process
+  const AreaOfTrapezoid = [(aBase + bBase) / 2] * height
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + AreaOfTrapezoid + ' mm²'
+}
